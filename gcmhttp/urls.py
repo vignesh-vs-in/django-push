9e7c2f906b4fc4ffc0d6b31e47536511d1502bd0
vignesh-vs-in <vignesh.vs.in@gmail.com>
2013-10-07 17:48:34 +0530

    Register view accepts a single reg_id argument and stores the data under GCUser table.

--- gcmhttp/urls.py
+++ gcmhttp/urls.py
@@ -5,4 +5,5 @@ from gcmhttp import views
 urlpatterns = patterns('',
 	# url(r'^$',views.index, name='index'),
 	url(r'^test$', views.testgcmhttp, name='testgcmhttp'),
+	url(r'^registeruser$', views.registeruser, name='registeruser'),
 )
